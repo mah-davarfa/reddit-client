adb7f4c412e391eb3d38cff0919788475987aba9
mah-davarfa <mah.davarfa@gmail.com>
2025-08-05 16:59:44 -0400

    Fix: import AppLayout.css globally in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './components/AppLayout.css';
 import App from './App.jsx'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
